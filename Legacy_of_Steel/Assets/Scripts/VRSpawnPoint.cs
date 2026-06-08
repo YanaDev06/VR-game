f41155f67a77694aa4d6d162468295238045eec5
YanaDev06 <yana.bystrova06bystrova@yandex.ru>
2026-06-08 12:44:05 +0300

    update

--- Legacy_of_Steel/Assets/Scripts/VRSpawnPoint.cs
+++ Legacy_of_Steel/Assets/Scripts/VRSpawnPoint.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
+using UnityEngine;
+using System.Collections;
 
 public class VRSpawnPoint : MonoBehaviour
 {
     private void Start()
     {
+       
+        StartCoroutine(TeleportWithDelay());
+    }
+
+    private IEnumerator TeleportWithDelay()
+    {
+        
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(0.1f);
+
         if (VRPlayerManager.Instance != null)
         {
             VRPlayerManager.Instance.TeleportTo(transform.position, transform.rotation);
+            Debug.Log($"Игрок телепортирован в {transform.position}");
+        }
+        else
+        {
+            Debug.LogError("VRPlayerManager не найден!");
         }
     }
 }
---
+++ Legacy_of_Steel/Assets/Scripts/XR_origin.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class XR_origin : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }  
+  
+}
